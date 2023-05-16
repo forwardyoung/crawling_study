5e109333df5a414f0712ff6326dd855a6b5698aa
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-05-16 19:19:32 +0900

    Update 크롤링.md

---
+++ selenium_insta/01_open_insta.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import chromedriver_autoinstaller
+import time
+
+chromedriver_autoinstaller.install()
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(3) # 3초까지 기다린다 => 에러 덜어줌
+
+url = "https://www.instagram.com/"
+driver.get(url=url)
+
+xpath = '//*[@id="loginForm"]/div/div[1]/div/label/input'
+# time.sleep(20)
